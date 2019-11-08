32ad20f1aec09c756c1547c3d49c7c7d8fc465ea
andr3wsw4g <andr3wsw4g@gmail.com>
2019-11-08 14:27:40 -0500

    Revision2

--- convolute.m
+++ convolute.m
@@ -1,9 +1,19 @@
-
 function Z = convolute(x, y)
-Z = zeros(length(x) + length(y) - 1,1);
-for c = 1:(length(x))
-    for d = 1:(length(y))
-        Z(c+d-1,1) = Z(c+d-1,1) +  0.01*x(c)*y(d);
+Z = zeros(1,length(x) + length(y) - 1);
+% Define x as a series of impulses
+% Define y[] as the set of shifted and scaled y's due to each impulse of x
+% Define z as the summation of shifted and scaled y's. AKA convolution.
+
+% Constant interval for sampling
+TIME_INTERVAL = 0.01;
+
+% Handles the convolution of each impulse in x with Yy
+for impulseOffset = 1:(length(x))
+    % Handles building z using shifted and scaled y
+    for indexY = 1:(length(y))
+        indexShiftedY = impulseOffset + indexY - 1;
+        scaleFactor = x(impulseOffset);
+        Z(1, indexShiftedY) = Z(1, indexShiftedY) +  TIME_INTERVAL * scaleFactor * y(indexY);
     end
 end
 end
